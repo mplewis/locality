76f20cc06f9e8c42b6b3ddd054661be287c3f992
Matthew Lewis <matt@mplewis.com>
2013-03-07 21:02:23 -0600

    moved most everything into separate js files; moved locations into its own js file. i'm so cool

---
+++ js/locations.js
@@ -0,0 +1,59 @@
+/* contains the locations at which matt might be
+ * example location format:
+ * 
+ * {
+ *      name: "Birthgivers' Home",
+ *      coordinates: {
+ *          latitude: 31.415926,
+ *          longitude: -53.832395
+ *      }
+ *  }
+ *
+ * make sure you put them in an array, because locations is an array, silly.
+ * the order in which they're placed in this array determines the order in
+ * which they're rendered on the page. so that's a thing too.
+ */
+var locations = [
+    {
+        name: "Stadium View",
+        coordinates: {
+            latitude: 44.971599,
+            longitude: -93.221807
+        }
+    },
+    {
+        name: "Kenneth H. Keller Hall",
+        coordinates: {
+            latitude: 44.974650,
+            longitude: -93.232460
+        }
+    },
+    {
+        name: "Mondale Hall",
+        coordinates: {
+            latitude: 44.97328,
+            longitude: -93.24436
+        }
+    },
+    {
+        name: "Centennial Hall",
+        coordinates: {
+            latitude: 44.97191,
+            longitude: -93.22952
+        }
+    },
+    {
+        name: "Coffman Union",
+        coordinates: {
+            latitude: 44.97284,
+            longitude: -93.23539
+        }
+    },
+    {
+        name: "Comstock Hall",
+        coordinates: {
+            latitude: 44.97232,
+            longitude: -93.23696
+        }
+    }
+];
